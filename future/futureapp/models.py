82bd84ea792d48c12912efe77a81628db9745efc
Tarik Tosun <ttosun@princeton.edu>
2012-03-30 00:06:38 -0400

    finished models except for permissions stuff.

--- future/futureapp/models.py
+++ future/futureapp/models.py
@@ -1,41 +1,42 @@
 from django.db import models
 
-
-
 class User(models.Model):
     CLASS_YEAR_CHOICES = ( 
-        (u'2012', u'Senior')
-        (u'2013', u'Junior')
+        (u'2012', u'Senior'),
+        (u'2013', u'Junior'),
         (u'2014', u'Sophomore')
     )
 
-    ADMIN_TYPE_CHOICES = (
-        (u'ME', u'Member')
-        (u'OF', u'Officer')
-        (u'FC', u'Food Chair')
-        (u'BAMF', u'Developer')
-    )
-
-    netid = models.CharField(max_length=8i, unique=True)
+    netid = models.CharField(max_length=8, unique=True)
     firstname = models.CharField("First Name", max_length=30)
     lastname = models.CharField("Last Name", max_length=30)
     year = models.IntegerField("Class Year", max_length=4, choices=CLASS_YEAR_CHOICES)
     fbid = models.BigIntegerField("Facebook ID", unique=True)
     authenticated = models.BooleanField("User Authenticated?")
     authcode = models.CharField("Authentication Code", max_length=30)
-    adminid = models.CharField("Administrator Type", max_length=4, choices=ADMIN_TYPE_CHOICES)
+
+class Admin(models.Model):
+    ADMIN_TITLE_CHOICES = (
+        (u'ME', u'Member'),
+        (u'OF', u'Officer'),
+        (u'FC', u'Food Chair'),
+        (u'BAMF', u'Developer')
+    )
+    title = models.CharField("Administrator Title", max_length=4, choices=ADMIN_TITLE_CHOICES)
+
+class Tag(models.Model):
+    text = models.CharField("Tag text", max_length=15)
 
 # post is a superclass for many kinds of things that are posted
 class Post(models.Model):
-	
-	author = models.ForeignKey(User, verbose_name="Post Author")
+    
+    author = models.ForeignKey(User, verbose_name="Post Author")
     time = models.DateTimeField(auto_now_add=True)
-	tags = models.ManyToManyField(Tag) 
-	mentions = models.ManyToManyField(Users) 
-	
-	class Meta:
-		abstract = True
-
+    tags = models.ManyToManyField(Tag) 
+    mentions = models.ManyToManyField(User, related_name="users_mentioned") 
+    
+    class Meta:
+        
 # general purpose user post, with a title
 class UserPost(Post):
     title = models.CharField("UserPost Title", max_length=80)
@@ -44,5 +45,5 @@ class UserPost(Post):
 # subordinate to other posts, no title
 class Comment(Post):
     text = models.TextField("Comment Text")
-	parent = models.ForeignKey(Post, verbose_name="Parent Post")
+    parent = models.ForeignKey(Post, verbose_name="Parent Post", related_name="parent_post")
 
