4475d3cd6e2027fd613b022657b5686cab1deed4
Tarik Tosun <ttosun@princeton.edu>
2012-04-06 17:41:33 -0400

    added post method to views.py

--- future/futureapp/views.py
+++ future/futureapp/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+
+# make a post.  first prototype.
+def post(request):
+    if request.method == 'POST':
+        newPost = UserPost(title = 'foo', #title=request.POST['title'],
+                     text = request.POST['text'],
+                     author = request.session['userid'],
+                     tags = ''
+                     mentions = '')
+        newPost.save()
+        return HttpResponse('woot.')
+    else:
+        return HttpResponse('SOMETHING IS WRONG.')
+
+        
+
+
