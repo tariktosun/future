cf887e15769957c558e9ff16edc06cbbd1153e48
Jeff Snyder <jasnyder@princeton.edu>
2012-04-13 15:26:07 -0400

    added path for templates to settings.py

--- future/settings.py
+++ future/settings.py
@@ -112,7 +112,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/tarik/cos333/future/future'
+    '/home/tarik/cos333/future/future',
+    '/Users/jasnyder/future',
 )
 
 INSTALLED_APPS = (
