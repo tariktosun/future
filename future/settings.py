d5ff20df579756b7a0ec15606bdb4d0d8e8490a7
Jeff Snyder <jasnyder@princeton.edu>
2012-05-15 01:48:07 -0400

    deleted unnecessary files

--- future/settings.py
+++ future/settings.py
@@ -4,12 +4,6 @@ from os import getenv
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -78,24 +72,11 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -105,7 +86,6 @@ SECRET_KEY = getenv('FUTURE_PYTHON_SECRET')
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -119,15 +99,10 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'future.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     getenv('FUTURE_DIR') + 'future/templates',
 )
 
-# temporarily..? commenting out default auth, trying to roll own
 INSTALLED_APPS = (
-#    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
@@ -135,31 +110,4 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'gunicorn',
     'future.futureapp',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
--- future/urls.py
+++ future/urls.py
@@ -3,19 +3,15 @@ from django.http import HttpResponse
 from futureapp.views import * 
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 # ----
-  # this function influenced by:
-  # https://github.com/semente/django-hashtags
+# this function influenced by:
+# https://github.com/semente/django-hashtags
 #-----
 hashtagged_url = url(
-                regex  = '^(?P<hashtag>[-\w]+)/$',
-                view = renderHashfiltered,
-                name = 'hashtagged_url'
-                )
+    regex  = '^(?P<hashtag>[-\w]+)/$',
+    view = renderHashfiltered,
+    name = 'hashtagged_url'
+    )
 
 urlpatterns = patterns('',
                        url(r'^$',drop),
@@ -37,18 +33,8 @@ urlpatterns = patterns('',
                        url(r'^user/(\w+-\w+)/$', renderProfile),
                        url(r'^search/$', search),
                        hashtagged_url,
-
-                       #url(r'^home/$', 'django.views.generic.simple.direct_to_template', {'template': 'future/static2.html'})
-                       # Examples:
-                           # url(r'^$', 'future.views.home', name='home'),
-                       # url(r'^future/', include('future.foo.urls')),
-
-                       # Uncomment the admin/doc line below to enable admin documentation:
-                           # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                       # Uncomment the next line to enable the admin:
-                           # url(r'^admin/', include(admin.site.urls)),
                        )
+
 urlpatterns += patterns('',
                         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
                         )
