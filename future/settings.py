7ee81379e6147c56a81b22697c08b43a2f05372b
Tarik Tosun <tariktosun@tarik_macbook.(none)>
2013-09-08 01:39:54 -0400

    set static root

--- future/settings.py
+++ future/settings.py
@@ -101,7 +101,10 @@ MEDIA_URL = '/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+if not HEROKU_PROJECT_DIR:
+    STATIC_ROOT = ''
+else:
+    STATIC_ROOT = os.path.join(HEROKU_PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
